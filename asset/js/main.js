7fd281e56a9604282e0285f8a4d4d8780082fe27
simoneluca00 <si.zaino@libero.it>
2022-03-10 10:50:19 +0100

    correzione (css da rivedere)

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,44 +1,41 @@
-// AVVISO DI COMPILARE I CAMPI SOLO CON TASTIERINO NUMERICO
+//1) AVVISO DI COMPILARE I CAMPI SOLO CON TASTIERINO NUMERICO
 alert("ATTENZIONE! Compilare i campi solo con tastierino numerico.")
 
-// km DA PERCORRERE
-let chilometriViaggio = prompt("Quanti chilometri vuoi percorrere?");
+//2) km DA PERCORRERE
+let chilometriViaggio = Number(prompt("Quanti chilometri vuoi percorrere?"));
 
-// ANNO CORRENTE
+//3) ANNO CORRENTE
 let annoCorrente = 2022;
 
-// ANNO DI NASCITA DEL PASSEGGERO
-let annoNascita = prompt("In che anno sei nato?");
+//4) ANNO DI NASCITA DEL PASSEGGERO
+let annoNascita = Number(prompt("In che anno sei nato?"));
 
-// ETA' DEL PASSEGGERO
-let etàPasseggero = (annoCorrente - annoNascita);
-console.log(etàPasseggero);
+//5) ETA' DEL PASSEGGERO
+let etaPasseggero = (annoCorrente - annoNascita);
 
-// MOLTIPLICAZIONE PER IL CALCOLO DEL PREZZO BASATO SUI km
-let prezzoStandard = chilometriViaggio * 0.21;
-let prezzoStandard2 = prezzoStandard.toFixed(2);
+//6) MOLTIPLICAZIONE PER IL CALCOLO DEL PREZZO BASATO SUI km
+let tassa = 0.21;
+let prezzoStandard = chilometriViaggio * tassa;
+let prezzoStandard2 = (prezzoStandard.toFixed(2));
 // let prezzoStandard2 = Math.round((prezzoStandard + Number.EPSILON)* 100) / 100; /-----/ (METODO ALTERNATIVO PER ARROTONDARE) 
-console.log(prezzoStandard2);
 
-// CALCOLO DEL PREZZO PER I MINORENNI (sconto del 20%) 
+//7) CALCOLO DEL PREZZO PER I MINORENNI (sconto del 20%) 
 let prezzoMinorenni = prezzoStandard - (( prezzoStandard * 20 ) / 100);
-let prezzoMinorenni2 = prezzoMinorenni.toFixed(2);
+let prezzoMinorenni2 = (prezzoMinorenni.toFixed(2));
 // let prezzoMinorenni2 = Math.round((prezzoMinorenni + Number.EPSILON)* 100) / 100; /-----/ (METODO ALTERNATIVO PER ARROTONDARE)
-console.log(prezzoMinorenni2);
 
-// CALCOLO DEL PREZZO PER GLI OVER 65 (sconto del 40%)
+//8) CALCOLO DEL PREZZO PER GLI OVER 65 (sconto del 40%)
 let prezzoAnziani = prezzoStandard - (( prezzoStandard * 40 ) / 100);
-let prezzoAnziani2 = prezzoAnziani.toFixed(2);
+let prezzoAnziani2 = (prezzoAnziani.toFixed(2));
 // let prezzoAnziani2 = Math.round((prezzoAnziani + Number.EPSILON)* 100) / 100; /-----/ (METODO ALTERNATIVO PER ARROTONDARE)
-console.log(prezzoAnziani2);
 
 
-// CONDIZIONI PER LO SCONTO IN BASE ALL'ETA'
-if ( etàPasseggero < 18 ) {
+//9) CONDIZIONI PER LO SCONTO IN BASE ALL'ETA'
+if ( etaPasseggero < 18 ) {
     document.getElementById("calculated_price").innerHTML = `Il versamento da effettuare per il tuo viaggio è di: ${prezzoMinorenni2}&euro; ( IVA Compresa ).`;
   }
 
-  else if ( etàPasseggero > 65 ) {
+  else if ( etaPasseggero > 65 ) {
     document.getElementById("calculated_price").innerHTML = `Il versamento da effettuare per il tuo viaggio è di: ${prezzoAnziani2}&euro; ( IVA Compresa ).`;
   }
 
